bf1fd5c920d68039e09fca2fa648b506ef7f2d9d
Heoyoon <parechae@gmail.com>
2023-05-23 22:45:42 +0900

    액체기초구현

---
+++ MyCooking/Assets/02.Scrips/LiquidObjects.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class LiquidObjects : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Transform Liquid;
+    private void OnEnable()
+    {
+        Liquid.DOScaleY(3, 2f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hit;
+        if (transform.rotation.z > 0.258819103)
+        {
+            if (Physics.Raycast(Liquid.position, -Liquid.up, out hit, 10, 128))
+            {
+                Debug.Log("��");
+                Debug.Log(hit.point);
+
+            }
+        }
+
+    }
+}
+
