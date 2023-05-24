25eed9a1359564c36e61052a502f9cc907d9864c
junhee4613 <junhee000123@naver.com>
2023-05-24 23:27:24 +0900

    인덕션 테스트용 색상 번경 구현

--- MyCooking/Assets/02.Scrips/UI/Induction.cs
+++ MyCooking/Assets/02.Scrips/UI/Induction.cs
@@ -2,21 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 public class Induction : MonoBehaviour
 {
     public bool fire = true;
     public int fireIndex = 0;
-    public Text text;
+    public TextMeshProUGUI text;
+    public Material inductionMAT;
+    public float inductionLayer;
     // Start is called before the first frame update        //인덕션에 있는 숫자를 fireIndex의 값으로 표시하기 위한 테스트용
     void Start()
     {
-        
+        /*text = GetComponent<TextMeshProUGUI>();*/             //나중에 주석 풀기
     }
 
     // Update is called once per frame
     void Update()
     {
-        text.text = "" + fireIndex;
+
+        text.text = fireIndex.ToString();
+        Debug.Log(fireIndex);
+
     }
     public void OnCollisionStay(Collision collision)
     {
@@ -26,14 +32,28 @@ public class Induction : MonoBehaviour
     {
         if (fire)
         {
-            fireIndex += 1;
+            
+            if (fireIndex <= 4)
+            {
+                fireIndex += 1;
+            }
+            inductionLayer = fireIndex * 0.2f;
+            inductionMAT.color = new Color(inductionLayer, 0, 0, 1);
         }
     }
     public void Down()
     {
         if (fire)
         {
-            fireIndex -= 1;
+            if (fireIndex >= 1)
+            {
+                fireIndex -= 1;
+            }
+            inductionLayer = fireIndex * 0.2f;
+            inductionMAT.color = new Color(inductionLayer, 0, 0, 1);
+
         }
+
+
     }
 }
